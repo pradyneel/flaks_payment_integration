5eec17ddcbfd4318a66a7dc9a7171e60a4c39a5f
pradyneel <pradyneel@gmail.com>
2021-12-07 14:38:20 +0530

    added file to github

---
+++ stripe/app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template, request
+import stripe
+
+stripe_keys = {
+  'secret_key': os.environ['STRIPE_SECRET_KEY'],
+  'publishable_key': os.environ['STRIPE_PUBLISHABLE_KEY']
+}
+
+stripe.api_key = stripe_keys['secret_key']
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in Inr
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        source=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='inr',
+        description='Flask Charge'
+    )
+
+    return render_template('charge.html', amount=amount)
+
+if __name__ == '__main__':
+    app.run(debug=True)
